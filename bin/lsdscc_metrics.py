7b82443cc4d53e676e938b3e5a12c4f3193f6329
cgsdfc <cgsdfc@126.com>
2019-03-29 22:44:32 +0800

    Generate and test on fake hypo.

--- bin/lsdscc_metrics.py
+++ bin/lsdscc_metrics.py
@@ -10,7 +10,6 @@ if __name__ == '__main__':
     parser.add_argument('--reference_file', '-r', help='custom reference corpus to use. (in json format)')
     args = parser.parse_args()
 
-    logging.basicConfig(level=logging.INFO)
     hypothesis_corpus = HypothesisSet.load_corpus(args.hypothesis_file, args.eos)
     reference_corpus = ReferenceSet.load_json_corpus(args.reference_file)
     score = compute_score_on_corpus(hypothesis_corpus, reference_corpus)
